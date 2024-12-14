948b824996501d86de1f7f6a3d5e6806ba0644f2
dsqnxvega33 <awaketoby@gmail.com>
2024-12-14 12:50:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["dotcom🩸", ".CØM"];
+const statusMessages = ["موت", "بعيدا عن "];
 let currentIndex = 0;
 
 async function login() {
